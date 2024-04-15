ab2794d1cf9ee618249a2857d6d8f133999c1b72
youssefLou <louzani1999@gmail.com>
2024-04-15 16:34:24 +0100

    first commit

---
+++ src/Layout.tsx
@@ -0,0 +1,325 @@
+import png from "./assets/png1.png";
+export const Layout = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-6 row-gap-10 lg:grid-cols-2">
+        <div className="relative">
+          <h1 className="font-bold text-5xl">
+            The <span className="text-blue-400"> 7 Success Pillars </span>
+          </h1>
+          <p className="font-medium text-lg">
+            My 7 Day Challenge walks you through how to go from zero to LIVE in
+            just 7 days.
+          </p>
+          <img className="h-auto w-auto" src={png} />
+        </div>
+        <div className="lg:py-6 lg:pr-16">
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 1</p>
+              <p className="font-bold text-xl">Watch This Video First</p>
+              <p className=" font-medium">
+                Gain an introduction to the strategy that Jordan used to achieve
+                $2,000/day in sales during his challenge.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 2</p>
+              <p className="font-bold text-xl">Finding A Winning Product</p>
+              <p className="font-medium">
+                Jordan walks you through exactly how to find a winning product
+                during this video.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 3</p>
+              <p className="font-bold text-xl">Building Your Store</p>
+              <p className=" font-medium">
+                Jordan shows you live examples of stores he's built and breaks
+                down the elements of them to help you build your own.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 4</p>
+              <p className="font-bold text-xl">Creating Your TikTok Ads</p>
+              <p className=" font-medium">
+                Make sure to take these steps in order to learn how to create a
+                viral TikTok Ad - the most important step in this process.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 5</p>
+              <p className="font-bold text-xl">TikTok Ads Testing Strategy</p>
+              <p className=" font-medium">
+                Learn exactly how to launch your TikTok Ads from Jordan's
+                testing strategy - if you've done everything else correctly, you
+                should get your first sale by the next day.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 6</p>
+              <p className="font-bold text-xl">Scaling Past $1,000/Day</p>
+              <p className=" font-medium">
+                Jordan breaks down exactly what to do next by breaking down his
+                scaling strategy live in this video.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-4 text-gray-600"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <line
+                      fill="none"
+                      strokeMiterlimit="10"
+                      x1="12"
+                      y1="2"
+                      x2="12"
+                      y2="22"
+                    />
+                    <polyline
+                      fill="none"
+                      strokeMiterlimit="10"
+                      points="19,15 12,22 5,15"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="w-px h-full bg-gray-300" />
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">DAY 7</p>
+              <p className="font-bold text-xl">What's Next</p>
+              <p className=" font-medium">
+                Now that you've earned some sales, here's how to take your
+                business to the next level.
+              </p>
+            </div>
+          </div>
+          <div className="flex py-10">
+            <div className="flex flex-col items-center mr-4">
+              <div>
+                <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                  <svg
+                    className="w-6 text-gray-600"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <polyline
+                      fill="none"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeMiterlimit="10"
+                      points="6,12 10,16 18,8"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="pt-1 pb-8">
+              <p className="mb-2 text-lg font-black text-blue-500">BONUS</p>
+              <p className="font-bold text-xl">$0 to $2000/Day Case Study</p>
+              <p className=" font-medium">
+                Jordan breaks down a never before seen case study of his own
+                $2,000/day store.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
