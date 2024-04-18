e86def01449935a996863fc9f2ae6e0123e73505
youssefLou <louzani1999@gmail.com>
2024-04-18 03:29:51 +0100

    Added pages Privacy and terms V2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,40 @@
+import { Link, Outlet } from "react-router-dom";
+
 function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-      <div className=" w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <ul className=" text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          <li className="py-4">
-            <a href="/privacy" className="  hover:underline me-4 md:me-6">
-              Privacy Policy
-            </a>
-          </li>
-          <li className="py-4">
-            <a href="/terms" className="hover:underline me-4 md:me-6">
-              Terms
-            </a>
-          </li>
-          <li className="py-4">
-            <a href="/" className="hover:underline me-4 md:me-6">
-              Home
-            </a>
-          </li>
-        </ul>
+    <>
+      <Outlet />
+      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <div className=" w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <ul className=" text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <li className="py-4">
+              <Link to="privacy" className="  hover:underline me-4 md:me-6">
+                Privacy Policy
+              </Link>
+            </li>
+            <li className="py-4">
+              <Link to="terms" className="hover:underline me-4 md:me-6">
+                Terms
+              </Link>
+            </li>
+            <li className="py-4">
+              <a href="/" className="hover:underline me-4 md:me-6">
+                Home
+              </a>
+            </li>
+          </ul>
 
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024{" "}
-          <a href="" className="hover:underline">
-            The 7-Day Challenge
-          </a>
-          . All Rights Reserved.
-        </span>
-      </div>
-    </footer>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <a href="" className="hover:underline">
+              The 7-Day Challenge
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home.tsx";
+import Privacy from "./components/Privacy.tsx";
+import Terms from "./components/Terms.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/Challenge/",
+    element: <App />,
+    children: [
+      {
+        path: "/Challenge/",
+        element: <Home />,
+      },
+      {
+        path: "/Challenge/privacy",
+        element: <Privacy />,
+      },
+      {
+        path: "/Challenge/terms",
+        element: <Terms />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
