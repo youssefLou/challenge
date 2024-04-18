be13ef1f87e40af5ead6ec7ced3ca6a335cdeae6
youssefLou <louzani1999@gmail.com>
2024-04-18 03:37:48 +0100

    Added pages Privacy and terms V2.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Privacy from "./components/Privacy";
-import Terms from "./components/Terms";
 
 function App() {
   return (
